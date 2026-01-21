04566e1dc394fc9619d33dad3237a03a981d5388
Mounib <mounib2812@outlook.com>
2026-01-21 14:31:55 +0100

    Enable framebuffer display and window resizing

--- src/main.cpp
+++ src/main.cpp
@@ -1,138 +1,165 @@
 #include <glad/gl.h>
+#include <stdint.h>
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
- 
-#include "linmath.h"
- 
-#include <stdlib.h>
+
 #include <stdio.h>
- 
-static const struct
-{
-    float x, y;
-    float r, g, b;
-} vertices[3] =
-{
-    { -0.6f, -0.4f, 1.f, 0.f, 0.f },
-    {  0.6f, -0.4f, 0.f, 1.f, 0.f },
-    {   0.f,  0.6f, 0.f, 0.f, 1.f }
+#include <stdlib.h>
+
+#include <array>
+
+/// @brief Minimum width of the program's window.
+static constexpr int WindowMinWidth = 640;
+/// @brief Minimum height of the program's window.
+static constexpr int WindowMinHeight = 480;
+
+/// @brief The emulator's native screen width (distinct from user window width).
+static constexpr int EmulatorFramebufferWidth = 256;
+/// @brief The emulator's native screen height (distinct from user window height).
+static constexpr int EmulatorFramebufferHeight = 224;
+
+struct Pixel {
+    uint8_t r, g, b, a;
 };
- 
-static const char* vertex_shader_text =
-"#version 110\n"
-"uniform mat4 MVP;\n"
-"attribute vec3 vCol;\n"
-"attribute vec2 vPos;\n"
-"varying vec3 color;\n"
-"void main()\n"
-"{\n"
-"    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
-"    color = vCol;\n"
-"}\n";
- 
-static const char* fragment_shader_text =
-"#version 110\n"
-"varying vec3 color;\n"
-"void main()\n"
-"{\n"
-"    gl_FragColor = vec4(color, 1.0);\n"
-"}\n";
- 
-static void error_callback(int error, const char* description)
-{
+
+/// @brief The emulator's framebuffer.
+std::array<Pixel, EmulatorFramebufferWidth * EmulatorFramebufferHeight> framebuffer;
+
+static const struct {
+    float x, y;
+    float u, v; // texture coordinates
+} framebufferQuad[4] = {
+    {-1.f, -1.f, 0.f, 1.f}, {-1.f, 1.f, 0.f, 0.f}, {1.f, 1.f, 1.f, 0.f}, {1.f, -1.f, 1.f, 1.f}};
+
+const unsigned int indices[] = {0, 1, 3, 1, 2, 3};
+
+static const char *vertex_shader_text = "#version 330 core\n"
+                                        "layout (location = 0) in vec2 aPos;\n"
+                                        "layout (location = 1) in vec2 aTexCoord;\n"
+                                        "out vec2 texCoord;\n"
+                                        "void main() {\n"
+                                        "   gl_Position = vec4(aPos.x, aPos.y, 0.0f, 1.0f);\n"
+                                        "   texCoord=aTexCoord;\n"
+                                        "}";
+
+static const char *fragment_shader_text = "#version 330 core\n"
+                                          "in vec2 texCoord;\n"
+                                          "out vec4 fragColor;\n"
+                                          "uniform sampler2D tex;\n"
+                                          "void main() {\n"
+                                          "   fragColor = texture(tex, texCoord);\n"
+                                          "}";
+
+static void error_callback(int error, const char *description) {
     fprintf(stderr, "Error: %s\n", description);
 }
- 
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
-{
+
+static void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods) {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GLFW_TRUE);
 }
- 
-int main(void)
-{
-    GLFWwindow* window;
-    GLuint vertex_buffer, vertex_shader, fragment_shader, program;
-    GLint mvp_location, vpos_location, vcol_location;
- 
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+    /* This variable determines whether the screen black bands (that exist because the window's
+     * dimensions ratio may be different from the framebuffer's ratio) are horizontal or vertical.
+     */
+    float scale = std::min((float)width / EmulatorFramebufferWidth,
+                           (float)height / EmulatorFramebufferHeight);
+
+    int viewportWidth = int(EmulatorFramebufferWidth * scale);
+    int viewportHeight = int(EmulatorFramebufferHeight * scale);
+
+    int viewportX = (width - viewportWidth) / 2;
+    int viewportY = (height - viewportHeight) / 2;
+
+    glViewport(viewportX, viewportY, viewportWidth, viewportHeight);
+}
+
+int main(void) {
+    GLFWwindow *window;
+    GLuint vertex_array, vertex_buffer, element_array, texture, vertex_shader, fragment_shader,
+        program;
+
     glfwSetErrorCallback(error_callback);
- 
+
     if (!glfwInit())
         exit(EXIT_FAILURE);
- 
+
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
- 
-    window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
-    if (!window)
-    {
+
+    window = glfwCreateWindow(WindowMinWidth, WindowMinHeight, "Simple example", NULL, NULL);
+    if (!window) {
         glfwTerminate();
         exit(EXIT_FAILURE);
     }
- 
+
     glfwSetKeyCallback(window, key_callback);
- 
+
     glfwMakeContextCurrent(window);
     gladLoadGL(glfwGetProcAddress);
     glfwSwapInterval(1);
- 
-    // NOTE: OpenGL error checks have been omitted for brevity
- 
+
+    int w, h;
+    glfwGetFramebufferSize(window, &w, &h);
+    framebuffer_size_callback(window, w, h);
+
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    glClearColor(1, 0, 0, 1);
+
+    glGenVertexArrays(1, &vertex_array);
+    glBindVertexArray(vertex_array);
+
     glGenBuffers(1, &vertex_buffer);
     glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
- 
+    glBufferData(GL_ARRAY_BUFFER, sizeof(framebufferQuad), framebufferQuad, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (const void *)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float),
+                          (const void *)(2 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    glGenBuffers(1, &element_array);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, element_array);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, EmulatorFramebufferWidth, EmulatorFramebufferHeight, 0,
+                 GL_RGBA, GL_UNSIGNED_BYTE, framebuffer.data());
+
     vertex_shader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
     glCompileShader(vertex_shader);
- 
+
     fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
     glCompileShader(fragment_shader);
- 
+
     program = glCreateProgram();
     glAttachShader(program, vertex_shader);
     glAttachShader(program, fragment_shader);
     glLinkProgram(program);
- 
-    mvp_location = glGetUniformLocation(program, "MVP");
-    vpos_location = glGetAttribLocation(program, "vPos");
-    vcol_location = glGetAttribLocation(program, "vCol");
- 
-    glEnableVertexAttribArray(vpos_location);
-    glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE,
-                          sizeof(vertices[0]), (void*) 0);
-    glEnableVertexAttribArray(vcol_location);
-    glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE,
-                          sizeof(vertices[0]), (void*) (sizeof(float) * 2));
- 
-    while (!glfwWindowShouldClose(window))
-    {
-        float ratio;
-        int width, height;
-        mat4x4 m, p, mvp;
- 
-        glfwGetFramebufferSize(window, &width, &height);
-        ratio = width / (float) height;
- 
-        glViewport(0, 0, width, height);
+
+    glUseProgram(program);
+
+    while (!glfwWindowShouldClose(window)) {
+
         glClear(GL_COLOR_BUFFER_BIT);
- 
-        mat4x4_identity(m);
-        mat4x4_rotate_Z(m, m, (float) glfwGetTime());
-        mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
-        mat4x4_mul(mvp, p, m);
- 
-        glUseProgram(program);
-        glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*) mvp);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
- 
+
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
- 
+
     glfwDestroyWindow(window);
- 
+
     glfwTerminate();
     exit(EXIT_SUCCESS);
 }
